b3df53cded0a5ed34cd17afe0afea49fd4e35663
macpanyvino <macpanyvino@gmail.com>
2022-06-30 10:08:37 -0700

    script 4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
-
  * main - Entry point
  *
  *  * Return: Always 0 (sucess)
@@ -13,7 +12,7 @@
 
 {
 
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
