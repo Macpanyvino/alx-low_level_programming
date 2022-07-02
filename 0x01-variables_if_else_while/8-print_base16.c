88529c67aa121766030b91765706b57d6e79d2ba
macpanyvino <macpanyvino@gmail.com>
2022-07-01 21:20:09 -0700

    task 8 update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,26 +13,25 @@ int main(void)
 {
 	char ch;
 
-       	int n;
+	int n;
 
-       		for (n = 48; n <= 57; n++)
+		for (n = 48; n <= 57; n++)
 
-        {
+		{
 
-		putchar(n);
+			putchar(n);
 
-	}
+		}
 
 		for (ch = 'a'; ch <= 'f'; ch++)
 
-	{
-
-	       	putchar(ch);
-
-	}
+		{
+			
+			putchar(ch);
+		{
        
 
-		putchar(10);
+			putchar(10);
 
 		return (0);
 
