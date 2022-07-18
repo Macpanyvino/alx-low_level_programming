def2f54b2c77853884c8740f48bbb8f8336feb58
macpanyvino <macpanyvino@gmail.com>
2022-07-18 08:42:26 -0700

    even more pointers, arrays and string task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* *_memcpy - copies memory area
-*
-* @dest: the string pointed to
-*
-* @src: the string to copy
-
-* @n: the size to print
-*
-* Return: pointer to dest.
-*/
+ * *_memcpy - copies memory area
+ *
+ * @dest: the string pointed to
+ *
+ * @src: the string to copy
+ *
+ * @n: the size to print
+ *
+ * Return: pointer to dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
